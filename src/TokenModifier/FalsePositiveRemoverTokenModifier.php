3f694f9b90c3358fed5a28c12cbfa88254fc2141
Michal Lulco <lulco@efabrica.sk>
2018-05-26 00:01:50 +0200

    Splitted method FalsePositiveRemoverTokenModifier::modify and removed coveralls from .travis.yml

--- src/TokenModifier/FalsePositiveRemoverTokenModifier.php
+++ src/TokenModifier/FalsePositiveRemoverTokenModifier.php
@@ -2,6 +2,7 @@
 
 namespace Efabrica\TranslationsAutomatization\TokenModifier;
 
+use Efabrica\TranslationsAutomatization\Tokenizer\Token;
 use Efabrica\TranslationsAutomatization\Tokenizer\TokenCollection;
 
 class FalsePositiveRemoverTokenModifier implements TokenModifierInterface
@@ -21,12 +22,17 @@ class FalsePositiveRemoverTokenModifier implements TokenModifierInterface
     {
         $newTokenCollection = new TokenCollection($tokenCollection->getFilePath());
         foreach ($tokenCollection->getTokens() as $token) {
-            foreach ($this->falsePositivePatterns as $falsePositivePattern) {
-                if (!preg_match($falsePositivePattern['block_pattern'], $token->getOriginalBlock()) || !preg_match($falsePositivePattern['text_pattern'], $token->getOriginalText())) {
-                    $newTokenCollection->addToken($token);
-                }
-            }
+            $this->addTokensToNewCollection($newTokenCollection, $token);
         }
         return $newTokenCollection;
+    }
+
+    private function addTokensToNewCollection(TokenCollection $newTokenCollection, Token $token): void
+    {
+        foreach ($this->falsePositivePatterns as $falsePositivePattern) {
+            if (!preg_match($falsePositivePattern['block_pattern'], $token->getOriginalBlock()) || !preg_match($falsePositivePattern['text_pattern'], $token->getOriginalText())) {
+                $newTokenCollection->addToken($token);
+            }
+        }
     }
 }
