1e6ca495d23f98bb601441633a5c6dbd502f9aef
Michal Lulco <lulco@efabrica.sk>
2018-06-18 23:42:05 +0200

    Changed config option to argument

--- src/Command/Extractor/ExtractorCommand.php
+++ src/Command/Extractor/ExtractorCommand.php
@@ -5,6 +5,7 @@ namespace Efabrica\TranslationsAutomatization\Command\Extractor;
 use Efabrica\TranslationsAutomatization\Exception\InvalidConfigInstanceReturnedException;
 use InvalidArgumentException;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -15,19 +16,19 @@ class ExtractorCommand extends Command
     {
         $this->setName('extract')
             ->setDescription('Finds non-translated texts, replaces them with translate tokens and store these texts to storage')
-            ->addOption('config', null, InputOption::VALUE_REQUIRED, 'Path to config file. Instance of ' . ExtractorConfig::class  . ' have to be returned')
-            ->addOption('params', null, InputOption::VALUE_REQUIRED, 'Params for config in format a=b&c=d');
+            ->addArgument('config', InputArgument::REQUIRED, 'Path to config file. Instance of ' . ExtractorConfig::class  . ' have to be returned')
+            ->addOption('params', null, InputOption::VALUE_REQUIRED, 'Params for config in format --params="a=b&c=d"');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        if (!is_file($input->getOption('config'))) {
-            throw new InvalidArgumentException('File "' . $input->getOption('config') . '" does not exist');
+        if (!is_file($input->getArgument('config'))) {
+            throw new InvalidArgumentException('File "' . $input->getArgument('config') . '" does not exist');
         }
         parse_str($input->getOption('params'), $params);
         extract($params);
 
-        $extractorConfig = require_once $input->getOption('config');
+        $extractorConfig = require_once $input->getArgument('config');
         if (!$extractorConfig instanceof ExtractorConfig) {
             throw new InvalidConfigInstanceReturnedException('"' . (is_object($extractorConfig) ? get_class($extractorConfig) : $extractorConfig) . '" is not instance of ' . ExtractorConfig::class);
         }
--- src/Command/Translator/TranslatorCommand.php
+++ src/Command/Translator/TranslatorCommand.php
@@ -5,6 +5,7 @@ namespace Efabrica\TranslationsAutomatization\Command\Translator;
 use Efabrica\TranslationsAutomatization\Exception\InvalidConfigInstanceReturnedException;
 use InvalidArgumentException;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -15,19 +16,19 @@ class TranslatorCommand extends Command
     {
         $this->setName('translate')
             ->setDescription('Creates new language version of translated texts')
-            ->addOption('config', null, InputOption::VALUE_REQUIRED, 'Path to config file. Instance of ' . TranslatorConfig::class . ' have to be returned')
+            ->addArgument('config', InputArgument::REQUIRED, 'Path to config file. Instance of ' . TranslatorConfig::class  . ' have to be returned')
             ->addOption('params', null, InputOption::VALUE_REQUIRED, 'Params for config in format a=b&c=d');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        if (!is_file($input->getOption('config'))) {
-            throw new InvalidArgumentException('File "' . $input->getOption('config') . '" does not exist');
+        if (!is_file($input->getArgument('config'))) {
+            throw new InvalidArgumentException('File "' . $input->getArgument('config') . '" does not exist');
         }
         parse_str($input->getOption('params'), $params);
         extract($params);
 
-        $translatorConfig = require_once $input->getOption('config');
+        $translatorConfig = require_once $input->getArgument('config');
         if (!$translatorConfig instanceof TranslatorConfig) {
             throw new InvalidConfigInstanceReturnedException('"' . (is_object($translatorConfig) ? get_class($translatorConfig) : $translatorConfig) . '" is not instance of ' . TranslatorConfig::class);
         }
