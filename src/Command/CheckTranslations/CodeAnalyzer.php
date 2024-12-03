dfa3cb887257696c9ce78f649c0e274189a06d5e
pobis <anton.pobis@gmail.com>
2024-12-03 12:03:16 +0100

    Add CheckTranslationsCommand, add CheckDirectories config_api

--- src/Command/CheckTranslations/CodeAnalyzer.php
+++ src/Command/CheckTranslations/CodeAnalyzer.php
@@ -35,13 +35,37 @@ class CodeAnalyzer
         $translateCalls = [];
         $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directory));
         foreach ($files as $file) {
-            if ($file->isFile() && $file->getExtension() === 'php') {
-                $code = file_get_contents($file->getPathname());
+            if (!$file->isFile()) {
+                continue;
+            }
+            $code = file_get_contents($file->getPathname());
+            if ($file->getExtension() === 'php') {
                 $translateCalls[] = $this->analyzeCode($code, $file->getPathname());
+            }
+            if ($file->getExtension() === 'latte') {
+                $translateCalls[] = $this->findInLatte($file);
             }
         }
 
         return array_merge(...$translateCalls);
+    }
+
+    private function findInLatte($file): array
+    {
+        $translateCalls = [];
+        $regex = "/\{_'[^']*'\}/";
+        $content = file_get_contents($file->getPathname());
+        if (preg_match_all($regex, $content, $matches)) {
+            $lines = file($file->getPathname());
+            foreach ($matches[0] as $match) {
+                foreach ($lines as $lineNumber => $lineContent) {
+                    if (strpos($lineContent, $match) !== false) {
+                        $translateCalls[] = ['key' => substr($match, 3, -2), 'file' => $file->getPathname(), 'line' => $lineNumber + 1, 'method' => 'in_latte'];
+                    }
+                }
+            }
+        }
+        return $translateCalls;
     }
 
     private function analyzeCode(string $code, string $filePath): array
